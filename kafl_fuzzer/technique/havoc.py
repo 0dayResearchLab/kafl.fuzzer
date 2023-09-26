2c0d6f7bd983d79efdd661f58a8cd9cf801fa2d6
5angjun <best_collin@naver.com>
2023-09-27 01:15:49 +0900

    Limit in/out buffer length2

--- kafl_fuzzer/technique/havoc.py
+++ kafl_fuzzer/technique/havoc.py
@@ -174,8 +174,8 @@ def mutate_length(irp_list, index, func):
                 #candidates = rand.int()#get_interesting_list(inlength)
                 chosen = rand.int(inlength) #rand.select(candidates)
             
-            if chosen > 2**20:
-                chosen = 2**20
+            if chosen > 131068-100:
+                chosen = 131068-100
         return chosen
     
 
