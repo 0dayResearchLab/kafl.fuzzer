13094bb9796b5e12b3f07da5eaa6b0325624f54a
5angjun <best_collin@naver.com>
2023-09-27 12:22:21 +0900

    remove unused file

--- example/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kafl fuzz -w ./work --seed-dir ./seed -p 20 --t-soft 255 --purge
-~                                                                                                    
-~                                                                                                    
-~                                                                           
